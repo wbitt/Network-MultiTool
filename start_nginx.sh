66d1bdc01790f1a46efcca2800fae334fc70dd83
Kamran Azeem <kaz@praqma.net>
2017-01-30 11:19:07 +0100

    added a <BR> and </p> after container IP.

--- start_nginx.sh
+++ start_nginx.sh
@@ -3,8 +3,8 @@ sed -i '/If\ you\ read\ this/d' /usr/share/nginx/html/index.html
 HOSTNAME=$(hostname)
 CIP=$(ifconfig eth0 | grep -w inet | awk '{print $2}')
 
-echo "Container Hostname: ${HOSTNAME}" >> /usr/share/nginx/html/index.html
-echo "Container IP: ${CIP}" >> /usr/share/nginx/html/index.html
+echo "<p>Container Name: ${HOSTNAME}" >> /usr/share/nginx/html/index.html
+echo "Container IP: ${CIP} <BR></p>" >> /usr/share/nginx/html/index.html
 
 
 nginx -g "daemon off;"
